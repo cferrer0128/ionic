561b8272dee510cb9a2f8e20af497d54783e2a36
cferrer0128 <cruz.ferrer2@gmail.com>
2017-11-01 11:40:53 -0400

    cf555

--- src/app/services/task.services.ts
+++ src/app/services/task.services.ts
@@ -6,12 +6,45 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class TaskServices{
 
+ 
     constructor(private http: Http){
         console.log('Http Services ')
     }
 
+    setHeaders(verb: string) {
+        let headers =  new Headers();
+        let digest = window["digest"];
+       // console.log('Disgest..!!! ', digest);
+        //if ( document.getElementById('__REQUESTDIGEST')) {digest = document.getElementById('__REQUESTDIGEST').innerHTML;}
+
+            if(verb.length>0)
+                headers.set('Accept', 'application/xml;odata=verbose');
+            else headers.set('Accept', 'application/json;odata=verbose');
+
+            headers.set('X-RequestDigest', digest);
+            headers.set('Access-Control-Allow-Origin', '*');
+
+            switch (verb) {
+            case 'POST':
+                headers.set('Content-type', 'application/json;odata=verbose');
+                break;
+            case 'PUT':
+                headers.set('Content-type', 'application/json;odata=verbose');
+                headers.set('IF-MATCH', '*');
+                headers.set('X-HTTP-Method', 'MERGE');
+                break;
+            case 'DELETE':
+                headers.set('IF-MATCH', '*');
+                headers.set('X-HTTP-Method', 'DELETE');
+                break;
+        }
+
+        return headers;
+     }
     getTasks(){
-        return this.http.get('https://ionwebapi.azurewebsites.net/api/tasks')
+       
+       
+        return this.http.get('https://ionwebapi.azurewebsites.net/api/tasks/', { headers: this.setHeaders('') })
         .map((res:Response) => res.json());
         
     }
